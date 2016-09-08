13a22e2d7a72ddbf0de05217f15acfa9812f2926
LNoe-lzy <m15222505516@163.com>
2016-09-08 10:55:29 +0800

    构建图像组件。

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,28 +3,52 @@ require('styles/App.scss');
 
 import React from 'react';
 
-// // 获取图片相关的数据
-// let imageData = require('../data/imageData.json');
-//
-// // 利用自执行函数, 将图片名信息转化为url路径信息
-// imageData = ((imageDataArr) => {
-//   for (let i = 0, j = imageDataArr.length; i < j; i++) {
-//     let singleImageData = imageDataArr[i];
-//     singleImageData.imageUrl = require('../images/' + singleImageData.filename);
-//     imageDataArr[i] = singleImageData;
-//   }
-//   return imageDataArr;
-// })(imageData);
+// 获取图片相关的数据
+let imageData = require('../data/imageData.json');
+
+// 利用自执行函数, 将图片名信息转化为url路径信息
+imageData = ((imageDataArr) => {
+  for (let i = 0, j = imageDataArr.length; i < j; i++) {
+    let singleImageData = imageDataArr[i];
+    singleImageData.imageUrl = require('../images/' + singleImageData.filename);
+    imageDataArr[i] = singleImageData;
+  }
+  return imageDataArr;
+})(imageData);
+
+class ImgFigure extends React.Component {
+  render() {
+    return (
+      <figure className="img-figure">
+        <img src={this.props.data.imageUrl}
+             alt={this.props.data.title}
+        />
+        <figcaption>
+          <h2 className="img-title">
+            {this.props.data.title}
+          </h2>
+        </figcaption>
+      </figure>
+    );
+  }
+}
 
 class AppComponent extends React.Component {
   render() {
+
+    let controllerUnits = [],
+      imgFigures = [];
+    imageData.forEach((value, index) => {
+      imgFigures.push(<ImgFigure data={value}/>);
+    });
+
     return (
       <section className="stage">
         <section className="img-sec">
-
+          {imgFigures}
         </section>
         <nav className="controller-nav">
-
+          {controllerUnits}
         </nav>
       </section>
     );
